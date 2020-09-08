f439b18f06864a8af1b093170d2792b0050f81cb
ajilderda <mail@andrejilderda.nl>
2020-09-08 21:55:13 +0200

    test: Add mock responses

---
+++ src/lib/__mocks__/tasks.tsx
@@ -0,0 +1,10 @@
+import * as items from '../../../mocks/tasks.get.json';
+import * as completedItems from '../../../mocks/completedItems.get.json';
+
+export const getItems = () => {
+  return items;
+}
+
+export const getCompletedItems = () => {
+  return completedItems;
+}
