b9441a10603863d0ce5b1e976e54c81d2ecd725e
ajilderda <mail@andrejilderda.nl>
2020-09-08 22:02:54 +0200

    chore: Add types

--- src/model/responses.ts
+++ src/model/responses.ts
@@ -1,6 +1,6 @@
-export interface GetAllResponse {
+export interface GetAllCompletedResponse {
   projects: Projects;
-  items: Task[];
+  items: CompletedTask[];
 }
 
 export interface Projects {
@@ -21,7 +21,7 @@ export interface Project {
   shared: boolean;
 }
 
-export interface Task {
+export interface CompletedTask {
   id: number;
   user_id: number;
   content: string;
@@ -29,4 +29,36 @@ export interface Task {
   project_id: number;
   task_id: number;
   meta_data: null;
+}
+
+export interface Task {
+  id: number;
+  content: string;
+  checked: number;
+  project_id: number;
+  user_id: number;
+  in_history: number;
+  priority: number;
+  collapsed: number;
+  date_added: Date;
+  date_completed: null;
+  assigned_by_uid: null;
+  responsible_uid: null;
+  added_by_uid: null;
+  is_deleted: number;
+  sync_id: null;
+  parent_id: null;
+  child_order: number;
+  section_id: null;
+  labels: number[];
+  day_order: number;
+  due: Due;
+}
+
+export interface Due {
+  date: Date;
+  timezone: null;
+  string: string;
+  lang: string;
+  is_recurring: boolean;
 }
