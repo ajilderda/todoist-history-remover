cdf83126a2a45cfee455e5eed64cc73120a12491
ajilderda <mail@andrejilderda.nl>
2020-09-08 21:56:16 +0200

    test: Comment out defaults + minor setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { getItems } from './lib/tasks';
+import { getCompletedItems } from './lib/tasks';
+
+jest.mock('./lib/tasks');
+
+beforeEach(() => {
+  // console.log(getItems());
+  // console.log(getCompletedItems());
+})
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // const { getByText } = render(<App />);
+  // const linkElement = getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+});
+
+test('Completed Tasks', () => {
 });
